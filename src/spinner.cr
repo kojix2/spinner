ac349a25dc7662c45c630bcf5c00067753660273
kojix2 <2xijok@gmail.com>
2023-03-08 13:11:21 +0900

    Support outputting spinner to other than STDOUT Support Colorize::Object

--- src/spinner.cr
+++ src/spinner.cr
@@ -2,24 +2,27 @@ require "./spinner/*"
 
 class Spin
   property delay
-  property chars : Array(String)
-  property text : String
+  property chars : Array(String) | Array(Colorize::Object(String))
+  property text : String | Colorize::Object(String)
 
   CL    = STDOUT.tty? ? "\u001b[0G" : "\u000d \u000d"
   CLEAR = STDOUT.tty? ? "\u001b[2K" : "\u000d"
 
-  def initialize(@delay = 0.1, chars = Spinner::Charset[:pipe], @text = "")
+  def initialize(@delay = 0.1, chars = Spinner::Charset[:pipe], @text = "", output = STDOUT)
     @state = true
     @chars = chars.to_a
+    @output = output
   end
 
   def stop
     @state = false
-    print CLEAR
+    @output.print CLEAR
+    @output.flush
   end
 
   private def clear(count)
-    print CL * count
+    @output.print CL * count
+    @output.flush
   end
 
   def start
@@ -28,7 +31,8 @@ class Spin
       while @state
         chars = @chars[i % @chars.size]
 
-        print "#{chars} #{text}"
+        @output.print "#{chars} #{text}"
+        @output.flush
         # to_s for colorize
         clear(chars.to_s.size)
 
